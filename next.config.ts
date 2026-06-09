d5607b382c6805c1aed488c5692a4192c62c3bfd
devbalbuena <balbuenadexter2@gmail.com>
2026-06-09 23:00:31 +0800

    readying the system before deploying it

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  serverExternalPackages: ['puppeteer', 'puppeteer-core'],
+  experimental: {
+    serverComponentsExternalPackages: ['@google/generative-ai']
+  }
 };
 
 export default nextConfig;
