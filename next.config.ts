603c5f975db962b710d26369089addaf1d84786f
devbalbuena <balbuenadexter2@gmail.com>
2026-06-10 23:11:26 +0800

    fix the slow issue in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  serverExternalPackages: ['puppeteer', 'puppeteer-core'],
-  experimental: {
-    serverComponentsExternalPackages: ['@google/generative-ai']
-  }
+  serverExternalPackages: ['puppeteer', 'puppeteer-core', '@google/generative-ai']
 };
 
 export default nextConfig;
